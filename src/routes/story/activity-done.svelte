038e33c3b5e738e5344e64b25fcdc08373022f1f
RidanBulwark <44413352+RidanBulwark@users.noreply.github.com>
2022-05-08 01:41:13 +0200

    hehe

--- src/routes/story/activity-done.svelte
+++ src/routes/story/activity-done.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 </script>
 
 <div class="container">
 	<h1>Have you finished the quest?</h1>
 	<!-- QUEST -->
-	<button class="action-btn confirm">Onward to more adventures!</button>
+	<button
+		class="action-btn confirm"
+		on:click={() => {
+			goto('choose-path');
+		}}>Onward to more adventures!</button
+	>
 	<button class="action-btn confirm">Let's go home and rest.</button>
 </div>
 
